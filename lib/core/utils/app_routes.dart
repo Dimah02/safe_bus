98834636426da1a0ebc760b5a5ac6838c4a6a9c9
dimah02 <dimahmehdawi@gmail.com>
2025-04-20 22:55:22 +0300

    add assets

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,12 +1,10 @@
 import 'package:go_router/go_router.dart';
+import 'package:safe_bus/features/shared/login/presentation/views/splash_screen.dart';
 
 abstract class AppRouter {
   static final router = GoRouter(
     routes: [
-      // GoRoute(
-      //   path: "/",
-      //   builder: (context, state) => const SplashView(),
-      // ),
+      GoRoute(path: "/", builder: (context, state) => const SplashScreen()),
     ],
   );
 }
--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -1,7 +1,42 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class KColors {
   KColors._();
 
-  static const primaryColor = Colors.red;
+  static const background = Color(0xffF8F8FF);
+
+  static const greenPrimary = Color(0xff00814B);
+  static const greenSecondary = Color(0xff44C07D);
+  static const greenAccent = Color(0xffA6E2AD);
+  static const lightGreen = Color(0xffD5EACD);
+
+  static const orangePrimary = Color(0xffA34DDE);
+  static const purpleSecondary = Color(0xffCB7EFF);
+  static const purpleAccent = Color(0xffEAB9FF);
+  static const lightPurple = Color(0xffF5DFFF);
+
+  static const purplePrimary = Color(0xffD37000);
+  static const orangeSecondary = Color(0xffFF9131);
+  static const orangeAccent = Color(0xffFFC249);
+  static const lightOrange = Color(0xffFFE895);
+
+  static const bluePrimay = Color(0xff7A9DF6);
+  static const blueAccent = Color(0xffACC3FF);
+
+  static const strokColor = Color(0xffE6E6E6);
+
+  static const fontColor = Color(0xff101720);
+  static const fontBlue = Color(0xff007BFE);
+
+  static const black = Color(0xff171E1B);
+  static const darkGrey = Color(0xff4D4F4E);
+  static const grey = Color(0xff969696);
+  static const lightGrey = Color(0xffD1D1D1);
+
+  static const iconBackground = Color.fromRGBO(118, 118, 128, 0.12);
+  static const labelSecondary = Color.fromRGBO(60, 60, 67, 0.6);
+
+  static const routeRed = Color.fromRGBO(236, 56, 11, 0.98);
 }
