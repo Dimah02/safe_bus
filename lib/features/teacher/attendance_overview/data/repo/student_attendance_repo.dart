affad209cba133072f42cc445f3993cc8c6ee40a
Dimah Mehdawi <105394518+Dimah02@users.noreply.github.com>
2025-05-25 17:31:01 +0300

    add student attendance repo

--- lib/features/teacher/attendance_overview/data/repo/student_attendance_repo.dart
+++ lib/features/teacher/attendance_overview/data/repo/student_attendance_repo.dart
@@ -1,5 +1,5 @@
 import 'package:safe_bus/core/utils/http_client.dart';
-import 'package:safe_bus/features/teacher/attendance_overview/presentation/data/models/student_model/student_model.dart';
+import 'package:safe_bus/features/teacher/attendance_overview/data/models/student_model/student_model.dart';
 
 class StudentAttendanceRepo {
   static final StudentAttendanceRepo instance =
