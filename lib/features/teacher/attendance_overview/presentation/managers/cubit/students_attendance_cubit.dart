5e0857d619dd0232641b2e74405c7da51ed4d54b
Dimah Mehdawi <105394518+Dimah02@users.noreply.github.com>
2025-05-25 18:36:37 +0300

    add getStudentsAttendance method

--- lib/features/teacher/attendance_overview/presentation/managers/cubit/students_attendance_cubit.dart
+++ lib/features/teacher/attendance_overview/presentation/managers/cubit/students_attendance_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:safe_bus/features/teacher/attendance_overview/data/models/student_model/student_model.dart';
+import 'package:safe_bus/features/teacher/attendance_overview/data/repo/student_attendance_repo.dart';
 
 part 'students_attendance_state.dart';
 
 class StudentsAttendanceCubit extends Cubit<StudentsAttendanceState> {
   StudentsAttendanceCubit() : super(StudentsAttendanceInitial());
+
+  Future<void> getStudentsAttendance({required int busRouteId}) async {
+    emit(StudentsAttendanceLoading());
+    try {
+      List<StudentModel> students = await StudentAttendanceRepo.instance
+          .getStudents(busRouteId: busRouteId);
+      emit(StudentsAttendanceSuccess(students: students));
+    } catch (e) {
+      emit(StudentsAttendanceFailure(errMessage: e.toString()));
+    }
+  }
 }
